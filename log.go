8dc4a27debf112388b7643904666073a94d531af
alichaddad <ali.chaddad@magalix.com>
2021-06-10 09:33:31 +0200

    use magalix custom logging middleware

--- log.go
+++ log.go
@@ -4,12 +4,10 @@ package gologging
 import (
 	"fmt"
 	"io"
-	"log/syslog"
-	"os"
 
+	"github.com/MagalixTechnologies/core/logger"
 	"github.com/luraproject/lura/config"
 	"github.com/luraproject/lura/logging"
-	gologging "github.com/op/go-logging"
 )
 
 // Namespace is the key to look for extra configuration details
@@ -18,17 +16,7 @@ const Namespace = "github_com/devopsfaith/krakend-gologging"
 var (
 	// ErrEmptyValue is the error returned when there is no config under the namespace
 	ErrWrongConfig = fmt.Errorf("getting the extra config for the krakend-gologging module")
-	// DefaultPattern is the pattern to use for rendering the logs
-	LogstashPattern          = `{"@timestamp":"%{time:2006-01-02T15:04:05.000+00:00}", "@version": 1, "level": "%{level}", "message": "%{message}", "module": "%{module}"}`
-	DefaultPattern           = ` %{time:2006/01/02 - 15:04:05.000} %{color}▶ %{level:.6s}%{color:reset} %{message}`
-	ActivePattern            = DefaultPattern
-	defaultFormatterSelector = func(io.Writer) string { return ActivePattern }
 )
-
-// SetFormatterSelector sets the ddefaultFormatterSelector function
-func SetFormatterSelector(f func(io.Writer) string) {
-	defaultFormatterSelector = f
-}
 
 // NewLogger returns a krakend logger wrapping a gologging logger
 func NewLogger(cfg config.ExtraConfig, ws ...io.Writer) (logging.Logger, error) {
@@ -36,49 +24,20 @@ func NewLogger(cfg config.ExtraConfig, ws ...io.Writer) (logging.Logger, error) 
 	if !ok {
 		return nil, ErrWrongConfig
 	}
-	module := "KRAKEND"
-	loggr := gologging.MustGetLogger(module)
-
-	if logConfig.StdOut {
-		ws = append(ws, os.Stdout)
+	logLevel := logConfig.Level
+	switch logLevel {
+	case "info":
+		logger.Config(logger.InfoLevel)
+	case "warn":
+		logger.Config(logger.WarnLevel)
+	case "debug":
+		logger.Config(logger.DebugLevel)
+	case "error":
+		logger.Config(logger.ErrorLevel)
+	default:
+		return nil, fmt.Errorf("Unsupported log level %s", logLevel)
 	}
-
-	if logConfig.Syslog {
-		var err error
-		var w *syslog.Writer
-		w, err = syslog.New(syslog.LOG_CRIT, logConfig.Prefix)
-		if err != nil {
-			return nil, err
-		}
-		ws = append(ws, w)
-	}
-
-	if logConfig.Format == "logstash" {
-		ActivePattern = LogstashPattern
-		logConfig.Prefix = ""
-	}
-
-	if logConfig.Format == "custom" {
-		ActivePattern = logConfig.CustomFormat
-		logConfig.Prefix = ""
-	}
-
-	backends := []gologging.Backend{}
-	for _, w := range ws {
-		backend := gologging.NewLogBackend(w, logConfig.Prefix, 0)
-		pattern := defaultFormatterSelector(w)
-		format := gologging.MustStringFormatter(pattern)
-		backendLeveled := gologging.AddModuleLevel(gologging.NewBackendFormatter(backend, format))
-		logLevel, err := gologging.LogLevel(logConfig.Level)
-		if err != nil {
-			return nil, err
-		}
-		backendLeveled.SetLevel(logLevel, module)
-		backends = append(backends, backendLeveled)
-	}
-
-	gologging.SetBackend(backends...)
-	return Logger{loggr}, nil
+	return Logger{}, nil
 }
 
 // ConfigGetter implements the config.ConfigGetter interface
@@ -92,68 +51,50 @@ func ConfigGetter(e config.ExtraConfig) interface{} {
 		return nil
 	}
 	cfg := Config{}
-	if v, ok := tmp["stdout"]; ok {
-		cfg.StdOut = v.(bool)
-	}
-	if v, ok := tmp["syslog"]; ok {
-		cfg.Syslog = v.(bool)
-	}
 	if v, ok := tmp["level"]; ok {
 		cfg.Level = v.(string)
-	}
-	if v, ok := tmp["prefix"]; ok {
-		cfg.Prefix = v.(string)
-	}
-	if v, ok := tmp["format"]; ok {
-		cfg.Format = v.(string)
-	}
-	if v, ok := tmp["custom_format"]; ok {
-		cfg.CustomFormat = v.(string)
 	}
 	return cfg
 }
 
 // Config is the custom config struct containing the params for the logger
 type Config struct {
-	Level        string
-	StdOut       bool
-	Syslog       bool
-	Prefix       string
-	Format       string
-	CustomFormat string
+	Level string
 }
 
 // Logger is a wrapper over a github.com/op/go-logging logger
-type Logger struct {
-	logger *gologging.Logger
-}
+type Logger struct{}
 
 // Debug implements the logger interface
 func (l Logger) Debug(v ...interface{}) {
-	l.logger.Debug(v...)
+	logger.Debug(v...)
 }
 
 // Info implements the logger interface
 func (l Logger) Info(v ...interface{}) {
-	l.logger.Info(v...)
+	logger.Info(v...)
 }
 
 // Warning implements the logger interface
 func (l Logger) Warning(v ...interface{}) {
-	l.logger.Warning(v...)
+	logger.Warn(v...)
 }
 
 // Error implements the logger interface
 func (l Logger) Error(v ...interface{}) {
-	l.logger.Error(v...)
+	logger.Error(v...)
 }
 
 // Critical implements the logger interface
 func (l Logger) Critical(v ...interface{}) {
-	l.logger.Critical(v...)
+	logger.Error(v...)
 }
 
 // Fatal implements the logger interface
 func (l Logger) Fatal(v ...interface{}) {
-	l.logger.Fatal(v...)
+	logger.Fatal(v...)
+}
+
+func (l Logger) InfoWithArgs(msg string, v ...interface{}) {
+	logger.Infow(msg, v...)
 }
---
+++ request_log.go
@@ -0,0 +1,81 @@
+package gologging
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+	"github.com/luraproject/lura/config"
+	"github.com/luraproject/lura/logging"
+)
+
+const (
+	RequestLogNamespace  = "github_com/magalix/gin-logger"
+	RequestLogmoduleName = "gin-logger"
+)
+
+func NewRequestLogger(cfg config.ExtraConfig, logger logging.Logger, loggerConfig gin.LoggerConfig) gin.HandlerFunc {
+	v, ok := RequestLogConfigGetter(cfg).(RequestLogConfig)
+	if !ok {
+		return gin.LoggerWithConfig(loggerConfig)
+	}
+
+	loggerConfig.SkipPaths = v.SkipPaths
+	logger.Info(fmt.Sprintf("%s: total skip paths set: %d", RequestLogmoduleName, len(v.SkipPaths)))
+
+	loggerConfig.Output = ioutil.Discard
+	loggerConfig.Formatter = Formatter{logger.(Logger), v}.DefaultFormatter
+	return gin.LoggerWithConfig(loggerConfig)
+}
+
+type Formatter struct {
+	logger Logger
+	config RequestLogConfig
+}
+
+func (f Formatter) DefaultFormatter(params gin.LogFormatterParams) string {
+	logData := []interface{}{
+		"method", params.Method,
+		"endpoint", params.Path,
+		"StatusCode", params.StatusCode,
+		"duration", params.Latency,
+	}
+
+	f.logger.InfoWithArgs("Default logging", logData...)
+
+	return ""
+}
+
+func RequestLogConfigGetter(e config.ExtraConfig) interface{} {
+	v, ok := e[RequestLogNamespace]
+	if !ok {
+		return nil
+	}
+	tmp, ok := v.(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	cfg := defaultConfigGetter()
+	if skipPaths, ok := tmp["skip_paths"].([]interface{}); ok {
+		var paths []string
+		for _, skipPath := range skipPaths {
+			if path, ok := skipPath.(string); ok {
+				paths = append(paths, path)
+			}
+		}
+		cfg.SkipPaths = paths
+	}
+
+	return cfg
+}
+
+func defaultConfigGetter() RequestLogConfig {
+	return RequestLogConfig{
+		SkipPaths: []string{},
+	}
+}
+
+type RequestLogConfig struct {
+	SkipPaths []string
+}
